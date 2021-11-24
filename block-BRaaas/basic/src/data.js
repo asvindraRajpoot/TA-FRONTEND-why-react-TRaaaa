6ae6c0e53375eb9d1572bc99357951d714bc585b
asvindra <aswin1399904@gmail.com>
2021-11-24 09:48:57 +0530

    completed block-4 of Props and Types

---
+++ block-BRaaas/basic/src/data.js
@@ -0,0 +1,9 @@
+let fruits = [
+    { id: 'apple', value: '🍎 apple' },
+    { id: 'orange', value: '🍊 orange' },
+    { id: 'grape', value: '🍇 grape' },
+    { id: 'pear', value: '🍐 pear' },
+  ];
+
+
+  export default fruits;
