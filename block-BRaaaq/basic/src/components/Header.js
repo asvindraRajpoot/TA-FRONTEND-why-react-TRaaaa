71076be20c98c3a86ea98dee199f6dcc13b47d33
asvindra <aswin1399904@gmail.com>
2021-11-24 08:41:00 +0530

    Added PropTypes to Articles App

---
+++ block-BRaaaq/basic/src/components/Header.js
@@ -0,0 +1,24 @@
+import  "../styles/index.css"
+function Header(props){
+    return (
+        <>
+        <nav>
+            <a href="/" className="brand">Brand</a>
+        <ul>
+
+<li>
+     Home
+                   
+</li>
+<li>About</li>
+<li>Contact</li>
+
+
+</ul>
+        </nav>
+
+        </>
+    );
+    
+}
+export default Header;
